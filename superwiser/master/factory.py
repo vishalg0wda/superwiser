760800454c947d3737c72e13b0e919dc7eacdebc
Vishal Gowda <cartmanboy1991@gmail.com>
2017-04-11 18:00:02 +0530

    Stash changes

--- superwiser/master/factory.py
+++ superwiser/master/factory.py
@@ -5,16 +5,17 @@ from superwiser.master.core import EyeOfMordor, Sauron
 
 
 class SauronFactory(object):
-    def make_sauron(self, conf_path, zk_host, zk_port, auto_redistribute):
+    def make_sauron(self, supervisor_conf, zk_host,
+                    zk_port, auto_redistribute):
         logger.info('Making Sauron')
-        if conf_path is None:
-            conf_path = os.path.join(os.getcwd(), 'aggregate.conf')
-        if not os.path.exists(conf_path):
-            open(conf_path, 'w').close()
+        if supervisor_conf is None:
+            supervisor_conf = os.path.join(os.getcwd(), 'aggregate.conf')
+        if not os.path.exists(supervisor_conf):
+            open(supervisor_conf, 'w').close()
         if zk_host is None:
             zk_host = os.environ.get('ZK_HOST', 'localhost')
         if zk_port is None:
             zk_port = os.environ.get('ZK_PORT', 2181)
         return Sauron(
-            conf_path,
+            supervisor_conf,
             EyeOfMordor(zk_host, zk_port, auto_redistribute))
--- superwiser/master/server.py
+++ superwiser/master/server.py
@@ -3,6 +3,7 @@ import optparse
 import requests
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, Factory
+from yaml import load
 
 from superwiser.master.factory import SauronFactory
 from superwiser.master.settings import MASTER_PORT
@@ -75,19 +76,41 @@ class SuperwiserFactory(Factory):
 
 def parse_opts():
     parser = optparse.OptionParser()
-    parser.add_option('-c', '--config', dest='conf_path')
-    parser.add_option('--zk-host')
-    parser.add_option('--zk-port')
+    parser.add_option('--master-conf', dest='master_conf')
+    parser.add_option('--supervisor-conf', dest='supervisor_conf')
+    parser.add_option('--zk-host', dest='zookeeper_host')
+    parser.add_option('--zk-port', 'zookeeper_port')
     parser.add_option('--auto-redistribute',
                       action='store_true',
-                      default=False)
-    options, _ = parser.parse_args()
-    return options.__dict__
+                      default=False,
+                      dest='auto_redistribute_on_failure')
+    parser.add_option('--node-drop-callback',
+                      action='append',
+                      dest='node_drop_callback')
+    return parser.parse_args[0]
+
+
+def build_conf(options):
+    # Initializse conf
+    conf = {}
+    # Extract master conf
+    master_conf = options.master_conf
+    if master_conf:
+        conf = master_conf
+    # Apply specified parameters as overrides
+    for k, v
+    retur
+    # Build conf from master-conf
+    if parser.master_conf:
+        conf = load(open(parser.master_conf))
+    # Apply overrides
+
 
 
 def start_server():
+    config = build_conf(parse_opts())
     factory = SuperwiserFactory(
-        SauronFactory().make_sauron(**parse_opts()))
+        SauronFactory().make_sauron(**config))
     reactor.listenTCP(MASTER_PORT, factory)
     logger.info('Starting server now')
     reactor.addSystemEventTrigger('before', 'shutdown', factory.teardown)
